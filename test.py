efd2bc1605546b094b00dcceb0db87651f12e625
bulldra <bulldra@gmail.com>
2023-07-05 06:49:59 +0900

    api定義変更

--- test.py
+++ test.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 load_dotenv()
 api_key = os.environ["x-api-key"]
 
-url = "https://chatai-48uaurqq.an.gateway.dev/hello"
+url = "https://chatai-48uaurqq.an.gateway.dev/planning"
 header = {"x-api-key": api_key, "Content-Type": "application/json"}
-payload = {"name": "World"}
+payload = {"product": "MVNO携帯電話"}
 
 res = requests.post(url, headers=header, json=payload)
 print(res.text)
