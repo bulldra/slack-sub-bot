d8eb6266e2629ae7a080d5fe132ba74eaea4160f
bulldra <bulldra@gmail.com>
2024-02-18 20:06:55 +0900

    duckduckgo対応

--- src/agent/agent_idea.py
+++ src/agent/agent_idea.py
@@ -9,7 +9,9 @@ from openai.types.chat import (
     ChatCompletionUserMessageParam,
 )
 
+import utils.duckduckgo_utils as duckduckgo_utils
 import utils.google_trends_utils as google_trends_utils
+import utils.slack_link_utils as slack_link_utils
 from agent.agent_gpt import AgentGPT
 from function.generative_synonyms import GenerativeSynonyms
 
@@ -24,89 +26,65 @@ class AgentIdea(AgentGPT):
         | ChatCompletionToolMessageParam
         | ChatCompletionFunctionMessageParam
     ]:
-        max_search_length: int = 3
-        conetnt: str = chat_history[-1]["content"].strip()
-        keywords: [str] = GenerativeSynonyms().generate(conetnt)
+        prompt_messages: [dict[str, str]] = []
 
-        # キーワード指定がない場合はトレンドワードを取得
+        keywords: [str] = GenerativeSynonyms().generate(chat_history)
         if keywords is None or len(keywords) == 0:
             keywords = [google_trends_utils.get_ramdom_trend_word()]
-
-        keyword_query = " OR ".join(keywords)
+        keyword_query: str = " OR ".join(keywords)
         query: str = f"{keyword_query} is:thread in:<#{self._share_channel}>"
-        self._logger.debug("query=%s", query)
-        prompt_messages: [dict[str, str]] = []
-        for message in self.extract_messages(query, max_search_length):
-            prompt_messages.extend(message)
+        for message in self.extract_messages(query, 3):
+            prompt_messages.append(message)
 
-        for message in self.extract_news(
-            keyword_query, max(max_search_length - len(prompt_messages), 1)
-        ):
-            prompt_messages.extend(message)
+        for site in duckduckgo_utils.scraping(query, 4 - len(prompt_messages)):
+            content: str = slack_link_utils.build_link(site.url, site.title)
+            content += "\n" + site.content
+            prompt_messages.append({"role": "assistant", "content": content})
+
+        prompt_messages.extend(chat_history)
 
         with open("./conf/idea_prompt.toml", "r", encoding="utf-8") as file:
             prompt = file.read()
             prompt = prompt.replace("${keyword}", keywords[0])
+            prompt_messages.append({"role": "user", "content": prompt.strip()})
 
-        prompt_messages.append({"role": "user", "content": prompt.strip()})
         return super().build_prompt(prompt_messages)
 
-    def extract_messages(self, query, num) -> []:
+    def extract_messages(self, query, num) -> dict[str, str] | None:
         serarch_result = self._slack_behalf_user.search_messages(
             query=query,
             count=50,
         )
         matches = serarch_result["messages"]["matches"]
-        self._logger.debug("matches=%s", len(matches))
+        self._logger.debug("query=%s, matches=%s", query, len(matches))
         selected = matches
         if len(matches) > num:
             selected = random.sample(matches, num)
 
         for message in selected:
-            result_messages: [dict] = []
-            timestamp: str = message.get("ts")
-            if timestamp is not None:
-                thread = self.extract_thread_message(
-                    message["channel"]["id"], timestamp
-                )
-                if thread is not None:
-                    result_messages.extend(thread)
+            timestamp: str = message["ts"]
+            channel: str = message["channel"]["id"]
+            thread = self.extract_thread_message(channel, timestamp)
+            if thread is not None:
+                yield thread
 
-            if len(result_messages) == 0:
-                attachments = message.get("attachments")
-                if attachments is not None and len(attachments) > 0:
-                    attachment = attachments[0]
-                    title = attachment.get("title") or ""
-                    text = attachment.get("text") or ""
-                    url = attachment.get("original_url") or ""
-                    result_messages.append(
-                        {"role": "assistant", "content": f"<{url}|{title}>\n{text}"}
-                    )
-                else:
-                    text = message.get("text")
-                    if text is not None and text != "":
-                        result_messages.append({"role": "assistant", "content": text})
-            yield result_messages
+            attachments = message.get("attachments")
+            if attachments is not None and len(attachments) > 0:
+                attachment = attachments[0]
+                title = attachment.get("title") or ""
+                text = attachment.get("text") or ""
+                url = attachment.get("original_url") or ""
+                yield {"role": "assistant", "content": f"<{url}|{title}>\n{text}"}
 
-    def extract_thread_message(self, channel, timestamp) -> []:
+            yield {"role": "assistant", "content": message.get("text", "")}
+
+    def extract_thread_message(self, channel, timestamp) -> dict | None:
         history: dict = self._slack.conversations_replies(
             channel=channel, ts=timestamp, limit=1
         )
         reply_messages: [dict] = history.get("messages")
-        result_messages: [dict] = []
-        if reply_messages is not None and len(reply_messages) > 1:
-            reply_messages = sorted(reply_messages, key=lambda x: x["ts"])[1:]
-            for message in reply_messages:
-                role: str = "user"
-                if message.get("bot_id"):
-                    role = "assistant"
-                result_messages.append({"role": role, "content": message.get("text")})
-        return result_messages
-
-    def extract_news(self, keyword: str, num) -> []:
-        matches: [] = google_trends_utils.get_keyword_news(keyword, num)
-        for entry in matches:
-            title: str = entry.get("title", "")
-            link: str = entry.get("link", "")
-            summary: str = entry.get("summary", "")
-            yield [{"role": "assistant", "content": f"<{link}|{title}>\n{summary}"}]
+        if reply_messages is not None and len(reply_messages) >= 1:
+            text = reply_messages[0].get("text")
+            if text is not None and text != "":
+                return {"role": "assistant", "content": text}
+        return None
---
+++ src/utils/duckduckgo_utils.py
@@ -0,0 +1,26 @@
+from duckduckgo_search import DDGS
+
+import utils.scraping_utils as scraping_utils
+
+
+def search(query: str, num: int = 10) -> [dict[str, str]]:
+    search_results = []
+    ddgs = DDGS()
+    for r in ddgs.text(query, region="jp-jp", timelimit="y"):
+        search_results.append(r)
+        if len(search_results) >= num:
+            break
+    return search_results
+
+
+def url_list(query: str, num: int = 10) -> [str]:
+    search_results = search(query, num)
+    return [r["href"] for r in search_results]
+
+
+def scraping(query: str, num: int = 10) -> [scraping_utils.Site]:
+    sites: [scraping_utils.Site] = []
+    for url in url_list(query, num):
+        if scraping_utils.is_allow_scraping(url):
+            sites.append(scraping_utils.scraping(url))
+    return sites
